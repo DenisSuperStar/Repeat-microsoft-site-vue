df3095eb6a22bf45171aea535d14f23343ab9180
Denis Shklyarskij <den.shklyarskij@bk.ru>
2020-07-22 15:03:45 +0300

    Created the component and in this component added item-list child component

---
+++ src/components/PartnersListMedium.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <v-item-group>
+            <v-container fluid>
+                <v-row>
+                    <v-col
+                        v-for="n in 8"
+                        :key="n"
+                        cols="12"
+                        md="4"
+                    >
+                        <item-list :range="n" />
+                    </v-col>
+                </v-row>
+            </v-container>
+        </v-item-group>
+    </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import ItemList from '../components/ItemList.vue'
+
+Vue.component('item-list', ItemList);
+
+export default {}
+</script>
