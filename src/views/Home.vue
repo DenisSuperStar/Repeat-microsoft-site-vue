63c590d6f8207892e3b4c4ea97dec6a01dc7ea08
Denis Shklyarskij <den.shklyarskij@bk.ru>
2020-07-22 14:56:56 +0300

    Added ready components

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,8 @@
 <template>
     <div>
       <v-app>
-        <app-header></app-header>
+        <humburger></humburger>
+        <app-header class="hidden-sm-and-down"></app-header>
         <app-carousel></app-carousel>
         <company-description></company-description>
         <partners-list></partners-list>
@@ -14,8 +15,10 @@ import Vue from 'vue'
 import AppHeader from '../components/AppHeader.vue'
 import AppCarousel from '../components/Slider.vue'
 import CompanyDescription from '../components/CompanyDescription.vue'
-import PartnersList from '../components/PartnersList.vue'
+import PartnersList from '../components/PartnersListMedium.vue'
+import Humburger from '../components/Humburger.vue'
 
+Vue.component('humburger', Humburger);
 Vue.component('app-header', AppHeader);
 Vue.component('app-carousel', AppCarousel);
 Vue.component('company-description', CompanyDescription);
