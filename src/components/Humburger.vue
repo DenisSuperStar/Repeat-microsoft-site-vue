a923733480fb9eaae2f965e7063227cac6422392
Denis Shklyarskij <den.shklyarskij@bk.ru>
2020-07-22 14:58:49 +0300

    Created component and wrote code

---
+++ src/components/Humburger.vue
@@ -0,0 +1,85 @@
+<template>
+    <div>
+        <v-app-bar-nav-icon 
+            color="black"
+            @click="drawer = !drawer"
+        >
+        </v-app-bar-nav-icon>
+        <v-navigation-drawer
+            v-model="drawer"
+            app
+            color="blue-grey lighten-5"
+        >
+            <v-list dense>
+                <template v-for="(icon, id) in icons_names">
+                    <v-list-item link :key="id">
+                        <v-list-item-action>
+                            <v-icon color="black">
+                                $vuetify.icons.{{icon.pic}}
+                            </v-icon>
+                        </v-list-item-action>
+                        <v-list-item-content>
+                            <v-list-item-title>
+                                {{icon.name}}
+                            </v-list-item-title>
+                        </v-list-item-content>
+                    </v-list-item>
+                </template>
+            </v-list>
+            <template v-slot:append>
+                <div class="pa-2">
+                    <v-btn
+                        block
+                        dark
+                        @click="drawer = !drawer"
+                    >
+                        Скрыть панель
+                    </v-btn>
+                </div>
+            </template>
+        </v-navigation-drawer>
+    </div>
+</template>
+
+<script>
+export default {
+    data: () => ({
+        drawer: false,
+        icons_names: [
+            {
+                pic: 'microsoft', name: 'Microsoft'
+            },
+            {
+                pic: 'office', name: 'Office'
+            },
+            {
+                pic: 'window', name: 'Windows'
+            },
+            {
+                pic: 'xbox', name: 'Xbox'
+            },
+            {
+                pic: 'support', name: 'Поддержка'
+            },
+            {
+                pic: 'all_softwares', name: 'Все продукты компании'
+            },
+            {
+                pic: 'search', name: 'Поиск'
+            },
+            {
+                pic: 'shopping_cart', name: 'Корзина'
+            },
+            {
+                pic: 'account', name: 'Зарегистрироваться'
+            },
+            {
+                pic: 'login', name: 'Войти'
+            },
+            {
+                pic: 'logout', name: 'Выйти'
+            }
+        ]
+    })
+}
+</script>
