9140ec00c6084b7c305f51fb70e3f7a087c10447
Denis Shklyarskij <den.shklyarskij@bk.ru>
2020-07-24 19:10:00 +0300

    Created toolbar component

---
+++ src/components/Toolbar.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <v-toolbar
+            color="primary"
+            dark
+            flat
+        >
+            <v-toolbar-title>{{title}}</v-toolbar-title>
+        </v-toolbar>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        title: {
+            type: String,
+            required: true
+        }
+    }
+}
+</script>
