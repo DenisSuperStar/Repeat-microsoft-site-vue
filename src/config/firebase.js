99d8311a5947c33414739a59bd5ea999f7e0c876
Denis Shklyarskij <den.shklyarskij@bk.ru>
2020-07-24 09:19:59 +0300

    Wrote code of firebase scripts

---
+++ src/config/firebase.js
@@ -0,0 +1,10 @@
+export default {
+        apiKey: "AIzaSyBduzRmp2qdn-NXUYfq99PrdIoRg9NfMbw",
+        authDomain: "vuetify-firebase-app-678f1.firebaseapp.com",
+        databaseURL: "https://vuetify-firebase-app-678f1.firebaseio.com",
+        projectId: "vuetify-firebase-app-678f1",
+        storageBucket: "vuetify-firebase-app-678f1.appspot.com",
+        messagingSenderId: "840631482610",
+        appId: "1:840631482610:web:267e55d3c4c8fe6a9bf86b",
+        measurementId: "G-WTBTSSY7B1"
+}
