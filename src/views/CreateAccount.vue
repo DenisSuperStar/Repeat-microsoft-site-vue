198db7cc14eff6b79f08fde16f643e397f46c334
Denis Shklyarskij <den.shklyarskij@bk.ru>
2020-07-24 09:19:12 +0300

    Created account component and wrote code

---
+++ src/views/CreateAccount.vue
@@ -0,0 +1,140 @@
+<template>
+    <v-app>
+        <div>
+            <v-container
+                class="fill-height"
+                fluid
+            >
+                <v-row
+                    align="center"
+                    justify="center"
+                >
+                    <v-col
+                        cols="12"
+                        
+                        sm="8"
+                        md="6"
+                        lg="6"
+                        xl="6"
+                    >
+                        <v-card class="elevation-12">
+                            <v-toolbar
+                                color="primary"
+                                dark
+                                flat
+                            >
+                                <v-toolbar-title>Регистрация</v-toolbar-title>
+                            </v-toolbar>
+                            <v-card-text>
+                                <v-form
+                                    ref="form"
+                                    v-model="valid"
+                                    lazy-validation
+                                >
+                                    <v-text-field
+                                        v-model="email"
+                                        :rules="emailRules"
+                                        label="Ваш эл.адрес"
+                                        required
+                                    ></v-text-field>
+
+                                    <v-text-field
+                                        v-model="password"
+                                        :rules="passwordRules"
+                                        label="Ваш пароль"
+                                        required
+                                    ></v-text-field>
+                                    <template v-if="selectedLayout">
+                                        <v-card-actions>
+                                            <v-btn
+                                                :disabled="!valid"
+                                                color="primary"
+                                                @click.prevent="validate"
+                                            >
+                                                Зарегистрироваться
+                                            </v-btn>
+                                            <v-btn
+                                                color="primary"
+                                                @click.prevent="reset, signup"
+                                            >
+                                                Очистить
+                                            </v-btn>
+                                        </v-card-actions>
+                                    </template>
+                                    <template v-else>
+                                        <v-btn
+                                            :disabled="!valid"
+                                            color="primary"
+                                            class="mb-3"
+                                            @click.prevent="validate"
+                                            block
+                                        >
+                                            Зарегистрироваться
+                                        </v-btn>
+                                        <v-btn
+                                            color="primary"
+                                            @click.prevent="reset, signup"
+                                            block
+                                        >
+                                            Очистить
+                                        </v-btn>
+                                    </template>
+                                </v-form>
+                            </v-card-text>
+                        </v-card>
+                    </v-col>
+                </v-row>
+            </v-container>
+        </div>
+    </v-app>
+</template>
+
+<script>
+import * as firebase from 'firebase/app'
+
+export default {
+    data: () => ({
+        email: '',
+        password: '',
+        valid: true,
+        name: '',
+        passwordRules: [
+            v => !!v || 'Password is required',
+            v => (v && v.length <= 10) || 'Password must be less than 10 characters',
+        ],
+        emailRules: [
+            v => !!v || 'E-mail is required',
+            v => /.+@.+\..+/.test(v) || 'E-mail must be valid string',
+        ],
+        selectedLayout: true
+    }),
+    methods: {
+        signup: function() {
+            firebase.auth().createUserWithEmailAndPassword(this.email, this.password)
+                .then(account => {
+                    console.log(account);
+                })
+                .catch(function(error) {
+                    console.log(error);
+                })
+        },
+        validate () {
+            this.$refs.form.validate()
+        },
+        reset: function() {
+            this.$refs.form.reset()
+        },
+        watchWidth: function() {
+            this.width = window.innerWidth;
+
+            if (this.width < 350) {
+                this.selectedLayout = false;
+            } else this.selectedLayout = true;
+        }
+    },
+    created: function() {
+        window.addEventListener('resize', this.watchWidth);
+        this.watchWidth();
+    }
+}
+</script>
