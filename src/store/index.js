64796bdfa5124e94d7081fdb667244f1abbe26b8
Denis Shklyarskij <den.shklyarskij@bk.ru>
2020-07-24 09:12:46 +0300

    Wrote code of these central store

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,13 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import userModule from '../add_store/user.js'
+import generalModule from '../add_store/general.js'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+ modules: {
+   generalModule,
+   userModule
+ }
 })
