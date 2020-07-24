066c347d9700170e21d61ac2affd7f1efb3b45ea
Denis Shklyarskij <den.shklyarskij@bk.ru>
2020-07-24 09:25:45 +0300

    Added modules of central store

---
+++ src/add_store/general.js
@@ -0,0 +1,25 @@
+export default {
+    state: {
+        processing: false,
+        error: null
+    },
+    mutations: {
+        SET_PROCESSING(state, payload) {
+            state.processing = payload
+        },
+        SET_ERROR(state, payload) {
+            state.error = payload
+        },
+        CLEAN_ERROR(state) {
+            state.error = null
+        }
+    },
+    getters: {
+        GET_PROCESSING: state => {
+            return state.processing
+        },
+        GET_ERROR: state => {
+            return state.error
+        }
+    }
+}
---
+++ src/add_store/user.js
@@ -0,0 +1,22 @@
+export default {
+    state: {
+       /*user: {
+           isAutenticated: false,
+           usId: null
+       } */
+    },
+    mutations: {
+
+    },
+    actions: {
+        /*SIGN_UP() {
+            firebase.auth().createUserWithEmailAndPassword(email, password)
+                .then(user => {
+                    console.log(user);
+                })
+                .catch(function(error) {
+                    console.log(error);
+                });
+        }*/
+    }
+}
