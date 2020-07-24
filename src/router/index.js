f52c453120b926f65d3f44b2f5bf83cb4091cd16
Denis Shklyarskij <den.shklyarskij@bk.ru>
2020-07-24 09:11:43 +0300

    Wrote code of these routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Account from '../views/CreateAccount.vue'
+import LogInAccount from '../views/LogInAccount.vue'
+
 
 Vue.use(VueRouter)
 
@@ -11,12 +14,49 @@ Vue.use(VueRouter)
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/news',
+    name: 'project',
+    component: () => import('../views/ProjectNews.vue')
+  },
+  {
+    path: '/solutions',
+    name: 'business',
+    component: () => import('../views/SolutionsForBusiness.vue')
+  },
+  {
+    path: '/ideas',
+    name: 'idea',
+    component: () => import('../views/IdeaForBusiness.vue')
+  },
+  {
+    path: '/play',
+    name: 'games',
+    component: () => import('../views/PlayForEmployee.vue')
+  },
+  {
+    path: '/support',
+    name: 'feedback',
+    component: () => import('../views/SupportSection.vue')
+  },
+  {
+    path: '/release',
+    name: 'products',
+    component: () => import('../views/ProductsCompany.vue')
+  },
+  {
+    path: '/cart',
+    name: 'purchases',
+    component: () => import('../views/ItemsCart.vue')
+  },
+  {
+    path: '/account',
+    name: 'regist',
+    component: Account
+  },
+  {
+    path: '/login',
+    name: 'enter',
+    component: LogInAccount
   }
 ]
 
