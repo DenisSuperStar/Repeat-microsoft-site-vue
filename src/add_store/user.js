6aaa3d077750cb5bc41571731d32cbbc0ec1817d
Denis Shklyarskij <den.shklyarskij@bk.ru>
2020-07-25 00:13:52 +0300

    Removed changes

--- src/add_store/user.js
+++ src/add_store/user.js
@@ -1,22 +1,14 @@
 export default {
     state: {
-       /*user: {
-           isAutenticated: false,
-           usId: null
-       } */
+
+    },
+    getters: {
+
     },
     mutations: {
 
     },
     actions: {
-        /*SIGN_UP() {
-            firebase.auth().createUserWithEmailAndPassword(email, password)
-                .then(user => {
-                    console.log(user);
-                })
-                .catch(function(error) {
-                    console.log(error);
-                });
-        }*/
+        
     }
 }
