be121d0feb2774c199c595e1341411c61af598f3
Denis Shklyarskij <den.shklyarskij@bk.ru>
2020-07-25 19:27:19 +0300

    Created component show errors on the display

---
+++ src/components/HandleError.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+        <v-snackbar
+            v-model="hiddenSnackbar"
+            :timeout="4000"
+            color="error"
+            multi-line
+            bottom
+        >
+            {{errorMessage}} {{errorCode}}
+            <v-btn
+                color="red darken-1"
+                class="white--text"
+                elevation="4"
+                @click.prevent="clear_error"
+            >
+                Скрыть
+            </v-btn>    
+        </v-snackbar>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        errorCode: {
+            type: String,
+            required: true
+        },
+        errorMessage: {
+            type: String,
+            required: true
+        }
+    },
+    data: () => ({
+      hiddenSnackbar: true  
+    }),
+    methods: {
+        clear_error: function() {
+            this.hiddenSnackbar = false;
+        }
+    }
+}
+</script>
