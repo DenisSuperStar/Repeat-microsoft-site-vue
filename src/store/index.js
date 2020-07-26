96ea794c02979cfe801f1b8cec027f91137f9fba
Denis Shklyarskij <den.shklyarskij@bk.ru>
2020-07-26 21:22:07 +0300

    Update store

--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,14 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import authSettings from '../add_store/userAuthSettings.js'
 import commonSettings from '../add_store/generalSettings.js'
+import accountSettings from '../add_store/userAccountSettings.js'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
  modules: {
    authSettings,
-   commonSettings
+   commonSettings,
+   accountSettings
  }
 })
